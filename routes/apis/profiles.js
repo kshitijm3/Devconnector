af677998b15e56b98ea11098b2755a01bae45498
Kshitijm3 <kshitijmathur052@gmail.com>
2021-08-26 18:08:53 +0530

    routes

---
+++ routes/apis/profiles.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req,res) => res.send('profiles route'))
+
+ module.exports =  router
--- server.js
+++ server.js
@@ -7,8 +7,14 @@ app.get('/', (req,res) => res.send('API running'))
 
 const PORT = process.env.PORT || 5000 
 
-const connectdb = require('./config/db')
+
 
 connectDB()
+
+app.use('/api/users' , require('./routes/apis/users'))
+app.use('/api/auth' , require('./routes/apis/auth'))
+app.use('/api/profiles' , require('./routes/apis/profiles'))
+app.use('/api/posts' , require('./routes/apis/post'))
+
 app.listen(PORT , ()=> console.log(`server started on ${PORT}`))
 
